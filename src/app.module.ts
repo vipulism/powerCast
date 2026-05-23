f9afff038a6efa73ee8b47a7a63ae8e68286c0e8
Vipul <vipul0809@gmail.com>
2026-05-23 19:03:21 +0530

    chore: keep app module minimal

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { ScheduleModule } from '@nestjs/schedule';
 import { HealthModule } from './modules/health/health.module';
 import { WeatherModule } from './modules/weather/weather.module';
 import { UsageModule } from './modules/usage/usage.module';
 import { AnalyzerModule } from './modules/analyzer/analyzer.module';
-import { NotificationsModule } from './modules/notifications/notifications.module';
-import { JobsModule } from './modules/jobs/jobs.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    ScheduleModule.forRoot(),
     HealthModule,
     WeatherModule,
     UsageModule,
     AnalyzerModule,
-    NotificationsModule,
-    JobsModule,
   ],
 })
 export class AppModule {}
