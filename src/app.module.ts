e3be9c5354acf1503b1287cc38d41d18e0d6ff04
Vipul <vipul0809@gmail.com>
2026-05-23 22:42:01 +0530

    feat: wire summary module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,9 @@ import { HealthModule } from './modules/health/health.module';
 import { WeatherModule } from './modules/weather/weather.module';
 import { UsageModule } from './modules/usage/usage.module';
 import { AnalyzerModule } from './modules/analyzer/analyzer.module';
+import { SummaryModule } from './modules/summary/summary.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    HealthModule,
-    WeatherModule,
-    UsageModule,
-    AnalyzerModule,
-  ],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), HealthModule, WeatherModule, UsageModule, AnalyzerModule, SummaryModule],
 })
 export class AppModule {}
