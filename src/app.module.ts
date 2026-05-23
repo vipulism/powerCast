3e3cf0ed2f5123f9043fd8c5eba7337f22606677
Vipul <vipul0809@gmail.com>
2026-05-23 18:59:01 +0530

    chore: add app module

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ScheduleModule } from '@nestjs/schedule';
+import { HealthModule } from './modules/health/health.module';
+import { WeatherModule } from './modules/weather/weather.module';
+import { PowerModule } from './modules/power/power.module';
+import { AnalyzerModule } from './modules/analyzer/analyzer.module';
+import { NotificationsModule } from './modules/notifications/notifications.module';
+import { JobsModule } from './modules/jobs/jobs.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    ScheduleModule.forRoot(),
+    HealthModule,
+    WeatherModule,
+    PowerModule,
+    AnalyzerModule,
+    NotificationsModule,
+    JobsModule,
+  ],
+})
+export class AppModule {}
