1926038949aeeb5f90dd1adf4293481bb41f4fcb
Vipul <vipul0809@gmail.com>
2026-05-23 23:11:39 +0530

    feat: enable scheduled summary module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,23 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ScheduleModule } from '@nestjs/schedule';
 import { HealthModule } from './modules/health/health.module';
 import { WeatherModule } from './modules/weather/weather.module';
 import { UsageModule } from './modules/usage/usage.module';
 import { AnalyzerModule } from './modules/analyzer/analyzer.module';
 import { SummaryModule } from './modules/summary/summary.module';
+import { SchedulerModule } from './modules/scheduler/scheduler.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), HealthModule, WeatherModule, UsageModule, AnalyzerModule, SummaryModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    ScheduleModule.forRoot(),
+    HealthModule,
+    WeatherModule,
+    UsageModule,
+    AnalyzerModule,
+    SummaryModule,
+    SchedulerModule,
+  ],
 })
 export class AppModule {}
