0b5e0aef5af724d47463fcaa16414f95f972d419
Vipul <vipul0809@gmail.com>
2026-05-24 00:00:07 +0530

    feat: export alerts service

--- src/modules/alerts/alerts.module.ts
+++ src/modules/alerts/alerts.module.ts
@@ -1,11 +1,12 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { SummaryModule } from '../summary/summary.module';
 import { AlertsController } from './alerts.controller';
 import { AlertsService } from './alerts.service';
 
 @Module({
-  imports: [SummaryModule],
+  imports: [forwardRef(() => SummaryModule)],
   controllers: [AlertsController],
   providers: [AlertsService],
+  exports: [AlertsService],
 })
 export class AlertsModule {}
